f8c7ee0fad4f11ea0eff0932655a27d3b1651cd0
Anderson0017 <andeerson0017@gmail.com>
2020-07-17 08:29:56 -0300

    estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyels from './styles/global';
+
 const App: React.FC = () => {
-  return <h1>teste</h1>;
+  return (
+    <>
+      <GlobalStyels />
+      <h1>teste</h1>
+    </>
+  );
 };
 
 export default App;
