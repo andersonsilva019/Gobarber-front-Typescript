fdd0410f1b73800bb1aea42f98f0c0b080b642c6
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-12-26 17:06:00 -0300

    feat: Criando Header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { FiPower } from 'react-icons/fi';
+import { Container, Header, HeaderContent, Profile } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="Gobarber" />
+          <Profile>
+            <img
+              src="https://avatars.githubusercontent.com/u/52974388?v=4"
+              alt="Anderson Silva"
+            />
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>Anderson Silva</strong>
+            </div>
+          </Profile>
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
 };
 
 export default Dashboard;
